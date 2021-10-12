f860b62185215af8191ed42d412c2a750ca29f13
BuiVinh <kytu147258@gmail.com>
2021-10-12 18:26:17 +0800

    aloo

---
+++ api/Models/Movie.js
@@ -0,0 +1,20 @@
+const mongose = require("mongoose");
+
+// const MovieSchema = new mongoose.Schema(
+   // {
+//     title: { type: String, required: true, unique: true },
+//     desc: { type: String },
+//     img: { type: String },
+//     imgTitle: { type: String },
+//     imgSm: { type: String },
+//     trailer: { type: String },
+//     video: { type: String },
+//     year: { type: String },
+//     duration : { type: Number },
+//     genre: { type: String },
+//     isSeries: { type: Boolean, defaut: false },
+    // },
+// { timestamps: true }
+// );
+
+// module.exports = mongoose.model("Movie, UserShema");
