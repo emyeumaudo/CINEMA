01042ea78dd0d0020fd7319d8cedf1ad30957d9a
BuiVinh <kytu147258@gmail.com>
2021-10-13 18:07:30 +0800

    register login

--- api/index.js
+++ api/index.js
@@ -1,11 +1,41 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const authRoute = require('./routes/auth');
+const userRoute = require('./routes/users');
+const dotenv = require('dotenv');
+const movieRoute = require("./routes/movies");
+const listRoute = require("./routes/lists");
+
 const port = 5000
 
-app.get('/', (req, res) => {
-  res.send('Xin cơm')
-})
+
+dotenv.config();
+// MONGODB
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    // useCreateIndex: true,
+  })
+  .then(() => console.log("DB Connection Successfull"))
+  .catch((err) => {
+    console.error(err);
+  });
+
+
+
+// app.get('/', (req, res) => {
+//   res.send('Xin cơm')
+// })
+app.use(express.json());
+
+app.use('/api/auth', authRoute);
+app.use('/api/users', userRoute);
+// app.use("/api/movies", movieRoute);
+// app.use("/api/lists", listRoute);
+
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+  console.log("Backend server is running");
 })
